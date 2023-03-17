d66fae343e10885582259e838364bf6ca326dfbc
Jonaspng <jonaspng@hotmail.com>
2023-03-17 15:20:22 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
 <template>
   <div style='textAlign: center; padding: 2rem'>
     <h5>This website uses data from PokéAPI version 2.</h5>
-    <h5>Data may take around 1-2 minutes to load.</h5>
+    <h5>Data takes around 10 seconds to load.</h5>
+    <h5>Do not click on another region while data is still loading.</h5>
     <h5>Some Pokemon may be missing due to missing information on Poke2API.</h5>
   </div>
   
